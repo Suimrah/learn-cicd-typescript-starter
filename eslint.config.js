7c33914da6a7cd5a34d48ba5a3101b27ff8665ab
abelmarmd@gmail.com <abelmarmd@gmail.com>
2026-08-14 17:07:19 -0400

    link update

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  { ignores: ["dist/**", "coverage/**", "src/db/migrations/**"] },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ["src/**/*.ts"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: globals.node,
+    },
+  },
+);
